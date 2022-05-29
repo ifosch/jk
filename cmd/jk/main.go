1e05185b338ebec8b68b0614a604985268dee3e1
Ignasi Fosch <natx@y10k.ws>
2022-05-29 19:58:47 +0200

    Add jk command with list subcommand

---
+++ cmd/jk/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/ifosch/jk/pkg/command"
+)
+
+var usage = `Usage: jk command [options]
+
+A jk CLI focused in job management tasks.
+
+Commands:
+  list     Lists all jobs available
+`
+
+func usageAndExit(msg string) {
+	if msg != "" {
+		fmt.Fprint(os.Stderr, msg)
+		fmt.Fprintf(os.Stderr, "\n")
+	}
+
+	flag.Usage()
+	os.Exit(1)
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, usage)
+	}
+
+	var cmd *command.Command
+
+	if len(os.Args) == 1 {
+		usageAndExit("jk: You need to provide a command.\n")
+	}
+
+	switch os.Args[1] {
+	case "list":
+		cmd = command.NewListCommand()
+	default:
+		usageAndExit(fmt.Sprintf("jk: '%s' is not a jk comand.\n", os.Args[1]))
+	}
+
+	cmd.Init(os.Args[2:])
+	cmd.Run()
+}
